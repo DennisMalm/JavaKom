04536b797439fcdfc1638efb8f3a491f01bff2a4
DennisMalm <malm.dennis@gmail.com>
2020-02-11 22:22:39 +0100

    Updated inner classes of PokeModel

---
+++ src/main/java/com/company/PokemonController.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.nio.file.AtomicMoveNotSupportedException;
+import java.util.List;
+
+public class PokemonController {
+
+    private String name;
+    private List<Moves> listOfMoves;
+}
